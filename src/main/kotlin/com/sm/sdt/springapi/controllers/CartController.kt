01f01055b34e10e16b5fba1f668a2be61a4e5d1d
Shahadat SDT <developer.shahadat@gmail.com>
2025-10-09 12:15:27 +0600

    added deleteCartItems and clearCart in cart Controller and Cart Entity

--- src/main/kotlin/com/sm/sdt/springapi/controllers/CartController.kt
+++ src/main/kotlin/com/sm/sdt/springapi/controllers/CartController.kt
@@ -5,15 +5,12 @@ import com.sm.sdt.springapi.dtos.CartDto
 import com.sm.sdt.springapi.dtos.CartItemDto
 import com.sm.sdt.springapi.dtos.UpdateCartItemRequest
 import com.sm.sdt.springapi.entities.Cart
-import com.sm.sdt.springapi.entities.CartItem
 import com.sm.sdt.springapi.mapper.CartMapper
-import com.sm.sdt.springapi.repository.CartItemRepository
 import com.sm.sdt.springapi.repository.CartRepository
 import com.sm.sdt.springapi.repository.ProductRepository
 import jakarta.validation.Valid
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.util.UriComponentsBuilder
@@ -25,7 +22,6 @@ class CartController(
     private val cartRepository: CartRepository,
     private val cartMapper: CartMapper,
     private val productRepository: ProductRepository,
-    private val cartItemRepository: CartItemRepository
 ) {
 
     @PostMapping
@@ -33,13 +29,9 @@ class CartController(
         uriComponentsBuilder: UriComponentsBuilder
     ): ResponseEntity<CartDto> {
         val cart = Cart()
-        println(cart.id.toString())
         cartRepository.save(cart)
-        println(cart.id.toString())
         val cartDto = cartMapper.toDto(cart)
-        println(cartDto.id.toString())
         val uri = uriComponentsBuilder.path("carts/{id}").buildAndExpand(cartDto.id).toUri()
-
         return ResponseEntity.created(uri).body(cartDto)
     }
 
@@ -51,14 +43,7 @@ class CartController(
     ): ResponseEntity<CartItemDto> {
         val cart = cartRepository.getCartWithItems(cartId).orElse(null) ?: return ResponseEntity.notFound().build()
         val product = productRepository.findByIdOrNull(request.productId) ?: return ResponseEntity.badRequest().build()
-        var cartItem = cart.items.find { it.product!!.id == product.id }
-
-        if (cartItem != null) {
-            cartItem.quantity = cartItem.quantity!! + 1
-        } else {
-            cartItem = CartItem(cart = cart, product = product, quantity = 1)
-            cart.items.add(cartItem)
-        }
+        val cartItem = cart.addCartItem(product)
         cartRepository.save(cart)
         return ResponseEntity.status(HttpStatus.CREATED).body(cartMapper.toCartItemDto(cartItem))
     }
@@ -76,16 +61,44 @@ class CartController(
     fun updateCartItem(
         @PathVariable productId: Long,
         @PathVariable cartId: UUID,
-         @RequestBody @Valid request: UpdateCartItemRequest
+        @RequestBody @Valid request: UpdateCartItemRequest
     ): ResponseEntity<*> {
-        val cart = cartRepository.getCartWithItems(cartId).orElse(null) ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-            mapOf("error" to "Cart Not found")
-        )
-        val cartItem = cart.items.find { it.product!!.id == productId } ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+        val cart =
+            cartRepository.getCartWithItems(cartId).orElse(null) ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(
+                    mapOf("error" to "Cart Not found")
+                )
+        val cartItem = cart.getCartItems(productId) ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
             mapOf("error" to "Product Not found")
         )
         cartItem.quantity = request.quantity
         cartRepository.save(cart)
         return ResponseEntity.ok().body(cartMapper.toCartItemDto(cartItem))
+    }
+
+    @DeleteMapping("/{cartId}/items/{productId}")
+    fun deleteCartItem(
+        @PathVariable cartId: UUID,
+        @PathVariable productId: Long
+    ): ResponseEntity<*> {
+        val cart = cartRepository.getCartWithItems(cartId).orElse(null) ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(
+                    mapOf("error" to "Cart Not found")
+                )
+
+        cart.deleteItem(productId)
+        cartRepository.save(cart)
+        return ResponseEntity.noContent().build<Unit>()
+    }
+
+    @DeleteMapping("/{cartId}/items")
+    fun clearCart(@PathVariable cartId: UUID) : ResponseEntity<Void> {
+
+        val cart = cartRepository.getCartWithItems(cartId).orElse(null)?:return ResponseEntity.notFound().build()
+
+        cart.clearCartItems()
+        cartRepository.save(cart)
+        return ResponseEntity.noContent().build()
+
     }
 }
--- src/main/kotlin/com/sm/sdt/springapi/entities/Cart.kt
+++ src/main/kotlin/com/sm/sdt/springapi/entities/Cart.kt
@@ -13,14 +13,36 @@ class Cart(
     @Column(name = "id")
     var id: UUID? = null,
 
-
     @Column(name = "date_created", insertable = false, updatable = false)
     var dateCreated: LocalDate? = null,
 
-    @OneToMany(mappedBy = "cart", cascade = [(CascadeType.MERGE)], fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "cart", cascade = [(CascadeType.MERGE)], fetch = FetchType.EAGER,orphanRemoval = true)
     var items: MutableList<CartItem> = arrayListOf()
 ) {
 
     fun calculateTotalPrice(): BigDecimal = items.sumOf { it.getTotalPrice() }
+
+    fun addCartItem(product: Product): CartItem {
+
+        var cartItem = items.find { it.product!!.id == product.id }
+
+        if (cartItem != null) {
+            cartItem.quantity = cartItem.quantity!! + 1
+        } else {
+            cartItem = CartItem(cart = this, product = product, quantity = 1)
+            items.add(cartItem)
+        }
+        return cartItem
+    }
+
+    fun getCartItems(productId: Long): CartItem? {
+        return items.find { it.product?.id == productId }
+    }
+
+    fun deleteItem(productId: Long) {
+         items.removeIf { it.product?.id == productId }
+    }
+
+    fun clearCartItems() = items.clear()
 
 }
