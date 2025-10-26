5f60f6f78aeb7005bb225979f0ec5d5325a0358d
Shahadat SDT <developer.shahadat@gmail.com>
2025-10-26 14:55:29 +0600

    add order and OrderItem entities and migration files

---
+++ src/main/kotlin/com/sm/sdt/springapi/entities/Order.kt
@@ -0,0 +1,33 @@
+package com.sm.sdt.springapi.entities
+
+import jakarta.persistence.*
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+import org.hibernate.annotations.ColumnDefault
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "orders")
+class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long? = null
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    var customer: User? = null
+
+    @Column(name = "status")
+    var status: Status? = null
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    var createdAt: LocalDateTime? = null
+
+    @Column(name = "total_price")
+    var totalPrice: BigDecimal? = null
+
+    @OneToMany(mappedBy = "order")
+    var items: MutableSet<OrderItem> = mutableSetOf()
+}
---
+++ src/main/kotlin/com/sm/sdt/springapi/entities/Status.kt
@@ -0,0 +1,5 @@
+package com.sm.sdt.springapi.entities
+
+enum class Status {
+    PENDING, PAID, FAILED, CANCELED
+}
